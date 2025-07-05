b2e6c3f4ad4ef2e64cfe665a8c6852d42ce74059
JosefStips <stipsjosef@gmail.com>
2025-07-05 21:58:06 +0200

    init. additional settings pages

---
+++ app/(auth)/modals/settings/sendFeedback.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const UserInfo = () => {
+	return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default UserInfo;
